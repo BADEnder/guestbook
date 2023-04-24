79c61cfc6df3d85e2b4d3bacd0dd49e7ee147d0a
BADEnder <s10183001@gmail.com>
2023-04-24 23:00:17 +0800

    2023-04-24 _ 2: modify corsOptions

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,9 +1,21 @@
+require('dotenv').config()
+
 const whitelist = [
     'https://127.0.0.1', 
     'http://127.0.0.1',
     'https://localhost', 
     'http://localhost',
 ]
+
+if(process.env.DOMAIN_NAME) {
+    whitelist.push(`https://${process.env.DOMAIN_NAME}`)
+    whitelist.push(`http://${process.env.DOMAIN_NAME}`)
+}
+
+if(process.env.IP) {
+    whitelist.push(`https://${process.env.IP}`)
+    whitelist.push(`http://${process.env.IP}`)
+}
 
 const corsOptions = {
     origin: (origin, callback) => {
