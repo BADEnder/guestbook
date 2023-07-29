5e97d0ed573b4c5d415339c98f5243f370dc7578
BADEnder <s10183001@gmail.com>
2023-07-29 19:31:21 +0800

    2023-07-29_3 modify

--- database/alterTables.js
+++ database/alterTables.js
@@ -5,28 +5,28 @@ const all_query = {
     query1: 
     `
     BEGIN;
-    ALTER TABLE replybook ADD COLUMN create_time timestamp DEFAULT NOW();
-    ALTER TABLE replybook ADD COLUMN create_user character varying (100) DEFAULT 'SYSTEM';
-    ALTER TABLE replybook ADD COLUMN update_time timestamp DEFAULT NOW();
-    ALTER TABLE replybook ADD COLUMN update_user character varying (100) DEFAULT 'SYSTEM';
+    ALTER TABLE replybook ADD create_time timestamp DEFAULT NOW();
+    ALTER TABLE replybook ADD create_user character varying (100) DEFAULT 'SYSTEM';
+    ALTER TABLE replybook ADD update_time timestamp DEFAULT NOW();
+    ALTER TABLE replybook ADD update_user character varying (100) DEFAULT 'SYSTEM';
     COMMIT;
     `,
     query2: 
     `
     BEGIN;
-    ALTER TABLE guestbook ADD COLUMN create_time timestamp DEFAULT NOW();
-    ALTER TABLE guestbook ADD COLUMN create_user character varying (100) DEFAULT 'SYSTEM';
-    ALTER TABLE guestbook ADD COLUMN update_time timestamp DEFAULT NOW();
-    ALTER TABLE guestbook ADD COLUMN update_user character varying (100) DEFAULT 'SYSTEM';
+    ALTER TABLE guestbook ADD create_time timestamp DEFAULT NOW();
+    ALTER TABLE guestbook ADD create_user character varying (100) DEFAULT 'SYSTEM';
+    ALTER TABLE guestbook ADD update_time timestamp DEFAULT NOW();
+    ALTER TABLE guestbook ADD update_user character varying (100) DEFAULT 'SYSTEM';
     COMMIT;
     `,
     query3: 
     `
     BEGIN;
-    ALTER TABLE members ADD COLUMN create_time timestamp DEFAULT NOW();
-    ALTER TABLE members ADD COLUMN create_user character varying (100) DEFAULT 'SYSTEM';
-    ALTER TABLE members ADD COLUMN update_time timestamp DEFAULT NOW();
-    ALTER TABLE members ADD COLUMN update_user character varying (100) DEFAULT 'SYSTEM';
+    ALTER TABLE members ADD create_time timestamp DEFAULT NOW();
+    ALTER TABLE members ADD create_user character varying (100) DEFAULT 'SYSTEM';
+    ALTER TABLE members ADD update_time timestamp DEFAULT NOW();
+    ALTER TABLE members ADD update_user character varying (100) DEFAULT 'SYSTEM';
     COMMIT;
     `
 }
